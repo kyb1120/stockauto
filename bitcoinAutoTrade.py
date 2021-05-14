3b4b4285261a4db53a952dd2a4cc3a83e9a1efdd
kyb1120 <81669361+kyb1120@users.noreply.github.com>
2021-05-14 10:28:15 +0900

    Create bitcoinAutoTrade.py

---
+++ bitcoinAutoTrade.py
@@ -0,0 +1,61 @@
+import time
+import pyupbit
+import datetime
+
+access = "MbJxZO51bdhllWH8KaXiObZJYOcouk6pJnivm9fH"
+secret = "Kn0u4hM2QhnoxuDbOVjs58edxxJ5SC9acef51QcM"
+
+def get_target_price(ticker, k):
+    """변동성 돌파 전략으로 매수 목표가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    return target_price
+
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
+
+# 로그인
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+
+# 자동매매 시작
+while True:
+    try:
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-ADA") # 9:00
+        end_time = start_time + datetime.timedelta(days=1) # 9:00 + 1일
+
+        # 9:00 < 현재 <8:59:50
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-ADA", 0.2)
+            current_price = get_current_price("KRW-ADA")
+            if target_price < current_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    upbit.buy_market_order("KRW-ADA", krw*0.9995)
+        else:
+            ada = get_balance("ADA")
+            if ada > 0.00008:
+                upbit.sell_market_order("KRW-ADA", ada*0.9995)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
